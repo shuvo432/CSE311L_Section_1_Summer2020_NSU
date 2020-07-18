9b56defc075f20f5914ca2cac42a30446be2102b
shuvo432 <mohaimenul.islam@northsouth.edu>
2020-07-17 22:14:29 -0600

    test commit

---
+++ Lab Asssessment Submission/lab assessment 2/create_db.php
@@ -0,0 +1,15 @@
+<?php
+$link = mysqli_connect('localhost','root','asddsa');
+if($link == false)
+{
+    die("Error: could not connect." .mysqli_connect_error());
+}
+$sql = "CREATE DATABASE VS_CODE_TEST";
+if(mysqli_query($link,$sql)){
+    echo"DB Created";
+}
+else{
+    echo"Error:Could not create db" .mysqli_connect_error($link);
+}
+mysqli_close($link)
+?>
